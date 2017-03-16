2737971d52fbf68c2ae9b0a8e90b99541717f924
Colander <jezek.malybuk@seznam.cz>
2017-03-16 21:01:37 +0100

    draws inactive connections with different colours

--- src/results_viewer/GraphDrawer.java
+++ src/results_viewer/GraphDrawer.java
@@ -1,5 +1,6 @@
 package results_viewer;
 
+import javafx.util.Pair;
 import neat.Genotype;
 import neat.NodeGene;
 import org.jbox2d.callbacks.DebugDraw;
@@ -14,7 +15,8 @@ import java.util.Random;
  * Created by colander on 15.3.17.
  */
 public class GraphDrawer {
-    ArrayList<Integer[]> lines = new ArrayList<>();
+    public static final Color3f GRAY_COLOR = new Color3f(0.2f, 0.2f, 0.2f);
+    ArrayList<Pair<Integer[], Boolean>> lines = new ArrayList<>();
     HashMap<Integer, Vec2> nodesById = new HashMap<>();
     final int X_OFFSET = 200;
     final int Y_OFFSET = 200;
@@ -33,12 +35,12 @@ public class GraphDrawer {
             }
             nodesById.put(nodeGene.innov, new Vec2(x, y));
         });
-        g.connectionGenes.forEach(connectionGene -> lines.add(new Integer[]{
+        g.connectionGenes.forEach(connectionGene -> lines.add(new Pair<>(new Integer[]{
                 ((int) nodesById.get(connectionGene.in).x),
                 ((int) nodesById.get(connectionGene.in).y),
                 ((int) nodesById.get(connectionGene.out).x),
                 ((int) nodesById.get(connectionGene.out).y),
-        }));
+        }, connectionGene.active)));
 
     }
 
@@ -47,18 +49,19 @@ public class GraphDrawer {
             debugDraw.drawString(new Vec2(nodesById.get(id).x, nodesById.get(id).y), id + "", Color3f.GREEN);
         }
         int i = 0;
-        for (Integer[] line : lines) {
+        for (Pair<Integer[], Boolean> line : lines) {
+            Integer[] coords = line.getKey();
             debugDraw.drawSegment(
-                    new Vec2(debugDraw.getScreenToWorld(line[0], line[1])),
-                    new Vec2(debugDraw.getScreenToWorld(line[2], line[3])),
-                    Color3f.GREEN);
-            int pointerX = (int) ((line[2] - line[0]) * (5.0 / 6) + line[0]);
-            int pointerY = (int) ((line[3] - line[1]) * (5.0 / 6) + line[1]);
+                    new Vec2(debugDraw.getScreenToWorld(coords[0], coords[1])),
+                    new Vec2(debugDraw.getScreenToWorld(coords[2], coords[3])),
+                    line.getValue() ? Color3f.GREEN : GRAY_COLOR);
+            int pointerX = (int) ((coords[2] - coords[0]) * (5.0 / 6) + coords[0]);
+            int pointerY = (int) ((coords[3] - coords[1]) * (5.0 / 6) + coords[1]);
             //debugDraw.drawCircle(debugDraw.getScreenToWorld(new Vec2(pointerX, pointerY)), debugDraw.get, Color3f.RED);
             debugDraw.drawSegment(
                     new Vec2(debugDraw.getScreenToWorld(pointerX, pointerY)),
-                    new Vec2(debugDraw.getScreenToWorld(line[2], line[3])),
-                    Color3f.RED);
+                    new Vec2(debugDraw.getScreenToWorld(coords[2], coords[3])),
+                    line.getValue() ? Color3f.RED : Color3f.WHITE);
             //debugDraw.drawSegment(new Vec2(line[0], line[1]), new Vec2(line[2], line[3]), Color3f.RED);
         }
     }
