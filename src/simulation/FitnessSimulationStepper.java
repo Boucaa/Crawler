b6e38dcb01217f3e30d8b3f0dbae22d7bd784042
Colander <jezek.malybuk@seznam.cz>
2017-03-22 14:01:12 +0100

    doc, angle limit

--- src/simulation/FitnessSimulationStepper.java
+++ src/simulation/FitnessSimulationStepper.java
@@ -125,6 +125,6 @@ public class FitnessSimulationStepper {
     }
 
     private double valueToAngle(double value) {
-        return (value - 0.5) * 2 * (Math.PI / 2);
+        return (value - 0.5) * 2  /* *(Math.PI / 2)*/;
     }
 }
