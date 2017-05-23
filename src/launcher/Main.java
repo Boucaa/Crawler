7022eadb0e3fa7d2bf3040d5a7277ebe9acd5da6
Colanderr <jezek.malybuk@seznam.cz>
2017-05-23 12:33:32 +0200

    body settings change

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -12,7 +12,7 @@ import worldbuilding.BodySettings;
 public class Main {
 
     public static void main(String[] args) {
-        BodySettings set = new BodySettings(4, 2, 6, 0.7f, 0.7f, 2.0f, 20.0f);
+        BodySettings set = new BodySettings(4, 2, 3, 0.7f, 0.7f, 2.0f, 20.0f);
         TestSettings.set(IOHandler.readFile("config.cfg"));
 
         for (int i = 0; i < 20; i++) {
